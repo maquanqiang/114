779c4b4124d32832f161c7789a896fa84480663a
Administrator <Administrator@SKY-20160913ICD>
2016-12-15 10:49:12 +0800

    first submit

---
+++ Singleton/src/test/Singleton.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Singleton {
+	private Singleton(){}
+	private static Singleton instance=new Singleton();
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
